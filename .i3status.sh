0ba896876aefc93010ef11988240aa94f95ecbf6
Joao Moreno <mail@joaomoreno.com>
2018-09-17 22:27:56 +0200

    remove hardcoded paths

--- .i3status.sh
+++ .i3status.sh
@@ -4,11 +4,11 @@ i3status | while :
 do
 	read INPUT
 	PLAYING=""
-	SPOTIFY=$(/home/joao/src/spotify-now/spotify-now -i "%artist - %title")
+	SPOTIFY=$($HOME/src/spotify-now/spotify-now -i "%artist - %title")
 	if [[ "$SPOTIFY" != "" ]]; then
 		PLAYING="$SPOTIFY | "
 	fi
-	RADIOTRAY=$(/home/joao/bin/radiotray-now-playing)
+	RADIOTRAY=$($HOME/bin/radiotray-now-playing)
 	if [[ "$RADIOTRAY" != "" ]]; then
 		PLAYING="$RADIOTRAY | "
 	fi
